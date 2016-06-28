25e0b85fa6b0ac2702543b5050779d007bb8eb4a
oakes <zsoakes@gmail.com>
2016-06-28 11:03:43 -0400

    Add exercise 7

---
+++ src/Exercise07.java
@@ -0,0 +1,13 @@
+/**
+ * Created by zach on 6/28/16.
+ */
+public class Exercise07 {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("racecar"));
+        System.out.println(isPalindrome("hello"));
+    }
+
+    public static boolean isPalindrome(String input) {
+        return false;
+    }
+}
